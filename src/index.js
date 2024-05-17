3cb3062625f0f66727cbd379701c03412106e639
preksha3005 <preksha.g504@gmail.com>
2024-05-17 21:29:48 +0530

    first commit

--- src/index.js
+++ src/index.js
@@ -1,17 +1,39 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
-
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+function Page() {
+  let [quote, setq] = React.useState(null);
+  let [count, setc] = React.useState(0);
+  function click() {
+    setc((prev) => prev + 1);
+  }
+  React.useEffect(() => {
+    fetch(
+      "https://gist.githubusercontent.com/camperbot/5a022b72e96c4c9585c32bf6a75f62d9/raw/e3c6895ce42069f0ee7e991229064f167fe8ccdc/quotes.json"
+    )
+      .then((response) => response.json())
+      .then((data) => {
+        let q = data.quotes;
+        let random = Math.floor(Math.random() * q.length);
+        setq(q[random].quote);
+      });
+  }, [count]);
+  return (
+    <div className="container">
+      <div className="box">
+        {quote && (
+          <div className="quote">
+            <p className="words">"{quote}"</p>
+          </div>
+        )}
+        <div className="button">
+          <button className="generate" onClick={click}>
+            Generate
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+}
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(<Page />);
